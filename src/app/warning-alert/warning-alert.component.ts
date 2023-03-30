c8a871d21a727f3a57fdc5e90c2fd6cfb3143ed9
Soumyajeet Satpathy <satpathysoumyajeet.99@gmail.com>
2023-03-30 15:32:06 +0530

    initial commit

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  // templateUrl: './warning-alert.component.html',
+  template: `
+    <p>This is a warning, DANGER!</p>
+  `,
+  styleUrls: ['./warning-alert.component.css']
+})
+export class WarningAlertComponent {
+
+}
